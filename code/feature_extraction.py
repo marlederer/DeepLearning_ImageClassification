dc6e46b54b08e4007660a1225892c7aa613563db
sagixflo <florian.sagmeister1505@gmail.com>
2024-02-14 15:44:43 +0100

    added 3-channel histogram as feature extraction

---
+++ code/feature_extraction.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+# creates a 3-channel histogram for each image which is used as a feature
+def create_three_channel_histograms(images, bins=256):
+    histograms = []
+    for image in images:
+        histograms.append([np.histogram(image[:, :, i], bins=bins, range=(0, 256))[0] for i in range(3)])
+    return np.array(histograms)
--- code/main.py
+++ code/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+from feature_extraction import create_three_channel_histograms
+
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -18,6 +20,23 @@ def show_images(images, labels, num_images=5):
         plt.axis('off')
     plt.show()
 
+def show_image_and_histogram(images, labels, histograms, bins=256, num_images=5):
+    fig, axes = plt.subplots(num_images, 4, figsize=(15, 4*num_images))
+
+    axes[0, 1].set_title('Red Histogram', color='red')
+    axes[0, 2].set_title('Green Histogram', color='green')
+    axes[0, 3].set_title('Blue Histogram', color='blue')
+
+    for i in range(num_images):
+        axes[i, 0].imshow(images[i])
+        axes[i, 0].set_title(labels[i])
+        axes[i, 0].axis('off')
+        for j, color in enumerate(['red', 'green', 'blue']):
+            axes[i, j + 1].bar(np.arange(len(histograms[i, j, :])), histograms[i, j, :], color=color, alpha=0.5)
+    plt.show()
+
+
+
 
 def get_labels_from_meta_file(file_path):
     meta_dict = unpickle(file_path)
@@ -25,7 +44,8 @@ def get_labels_from_meta_file(file_path):
     label_names = [name.decode('utf-8') for name in label_names]
     return label_names
 
-path = os.path.join(os.getcwd(), 'data', 'cifar_10', 'cifar-10-python', 'cifar-10-batches-py')
+
+path = os.path.join('..', 'data', 'cifar_10', 'cifar-10-python', 'cifar-10-batches-py')
 img_path = os.path.join(path, 'data_batch_1')
 meta_path = os.path.join(path, 'batches.meta')
 
@@ -38,7 +58,10 @@ Y = img_dict[b'labels']
 X = X.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).astype("uint8")
 Y = np.array(Y)
 
-# Display some random images
+img_histograms = create_three_channel_histograms(X)
+
+# Display some random images and their 3-channel histograms
 random_indices = np.random.randint(0, len(X), 5)
 labels = [label_names[label] for label in Y[random_indices]]
-show_images(X[random_indices],labels)
+show_image_and_histogram(X[random_indices], labels, img_histograms[random_indices])
+
